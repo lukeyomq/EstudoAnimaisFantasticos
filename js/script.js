f37104742c3c5473c109a046cc3fb7d0fdc09cff
lukeyomq <luke_1999queiroz@hotmail.com>
2023-03-22 10:24:14 -0300

    new update

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ import initAccordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js"
 import initTollTip from "./modules/tooltip.js";
 import initDropDownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -13,3 +16,6 @@ initAccordion();
 initModal();
 initTollTip();
 initDropDownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
